3d0d4d4d570caf298aa0b56f4bc26307c48db430
supersoimon <simon.gidrewicz@gmail.com>
2026-08-26 19:26:35 -0600

    Refactor keymap to include normal and function layers

--- CODE/keymaps/default/keymap.c
+++ CODE/keymaps/default/keymap.c
@@ -1,42 +1,28 @@
 #include QMK_KEYBOARD_H
 
+#define GRAVE_MODS  (MOD_BIT(KC_LSFT)|MOD_BIT(KC_RSFT)|MOD_BIT(KC_LGUI)|MOD_BIT(KC_RGUI)|MOD_BIT(KC_LALT)|MOD_BIT(KC_RALT))
+
 enum layer_names {
-    _BL,
+    normal,
+    function,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BL] = LAYOUT(
-                 KC_PGUP,            // SW5 (Top Middle)
-        KC_LEFT, KC_PGDN, KC_RGHT    // SW1, SW2, SW3 (Bottom Row)
-    )
+    [normal] = LAYOUT(
+      KC_ESC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MNXT, KC_KB_MUTE, KC_KB_VOLUME_DOWN, KC_KB_VOLUME_UP, KC_PSCR, KC_SCRL, KC_MPLY,
+      KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_INS, KC_HOME, KC_PGUP,
+      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, KC_END, KC_PGDN,
+      KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+      KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,
+      KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, MO(function), KC_MENU, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+    [function] = LAYOUT(
+      _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,_______,_______,_______,_______,_______,_______,
+      _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+      _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+      _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+      _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______
+    ),
 };
 
-#if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][1][2] = {
-    [_BL] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-};
-#endif
-
-#ifdef OLED_ENABLE
-bool oled_task_user(void) {
-    // Host Keyboard Layer Status
-    oled_write_P(PSTR("Layer: "), false);
-
-    switch (get_highest_layer(layer_state)) {
-        case _BL:
-            oled_write_P(PSTR("Default\n"), false);
-            break;
-        default:
-            // Or use the write_ln shortcut over adding '\n' to the end of your string
-            oled_write_ln_P(PSTR("Undefined"), false);
-    }
-
-    // Host Keyboard LED Status
-    led_t led_state = host_keyboard_led_state();
-    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
-    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
-    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
-    
-    return false;
-}
-#endif
